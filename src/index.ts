d42c37ad2a94904ead0fd7d3e191c2edea1b5ced
JSSatorres <juansataz.dev@gmail.com>
2024-07-05 11:32:49 +0200

    feat: add main router

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
 import express from 'express';
+import routes from './core/routes';
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(routes);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
